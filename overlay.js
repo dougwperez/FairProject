c16219371cd74d0c8e70bce11912c6aa0af54a1d
Doug Perez <douglas.perez@rackspace.com>
2023-09-01 15:10:53 -0700

    add error page

--- overlay.js
+++ overlay.js
@@ -2,8 +2,14 @@ var header = document.getElementById("welcome-screen-header").style;
 var bubbles = document.getElementById("bubbles-body").style;
 var screen1Array = document.getElementsByClassName("screen-1");
 var screen2Array = document.getElementsByClassName("screen-2");
+var errorScreenArray = document.getElementsByClassName("error-screen");
 var resetBtn = document.getElementsByClassName("close")[0];
 var searchInput = document.getElementById("searchText")
+var responseBody = document.getElementsByClassName("screen-2 body-text")[0]
+var responsePrompt = document.getElementsByClassName("screen-2 results-question")[0]
+var errorResponsePrompt = document.getElementsByClassName("error-screen results-question")[0]
+var errorResponseBody = document.getElementsByClassName("error-screen body-text")[0]
+var errorResponseBody2 = document.getElementsByClassName("error-screen body-text-header")[0]
 
 
 searchInput.addEventListener('input', function (event) {
@@ -53,6 +59,10 @@ function closeOverlay() {
   for (let item of screen2Array) {
     item.style.display = "none";
   }
+
+  for (let item of errorScreenArray) {
+    item.style.display = "none";
+  }
 }
 
 function fadeInResultsScreen() {
@@ -88,15 +98,20 @@ console.log('Koca: searchText ', searchText);
 function generateResponse() {
 
     console.log("searchInput", searchInput.value)
+    console.log("responseBody.innerHTML", responseBody.innerHTML)
 
   event.preventDefault()
   fadeOutWelcomeScreen();
 
-const url = "https://us-central1-fair-cdo-prj-6e5b.cloudfunctions.net/cf-fair-rss-query";
+const url = "https://us-central1-fair-cdo-prj-6e5b.cloudfunctions.net/cf-fr-rss-qry";
 const headers = {
   "Content-Type": "application/json",
 };
+
+//What are the top 10 priorities for CDOs?
 // What is 3-Step Guide for Data Leaders to Move from Hype to Results?
+
+
 const data = {
   "prompt": `${searchInput.value}`,
 };
@@ -114,11 +129,25 @@ async function fetchData() {
     if (response.ok) {
       const responseData = await response.json(); // Parse the JSON response
       console.log("Response data:", responseData);
+      responseBody.innerHTML = responseData.content;
+      responsePrompt.innerHTML = responseData.prompt;
+
     } else {
       throw new Error("Request failed with status: " + response.status);
     }
   } catch (error) {
     // Handle any errors that occurred during the fetch
+    errorResponsePrompt.innerHTML = searchInput.value;
+    errorResponseBody2.innerHTML = `No results found for: <b>${searchInput.value}</b>`
+
+    for (let item of screen2Array) {
+      item.style.display = "none";
+    }
+
+    for (let item of errorScreenArray) {
+      item.style.display = "inline";
+    }
+    
     console.error("Error:", error);
   }
 }
